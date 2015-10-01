35936c83362c3a94c1aba83cdd3921e6ba362425
Fakher Oueslati <oueslati.fakher@gmail.com>
2015-09-30 21:01:21 -0400

    Added receiving photo and text - Added receiving text and displaying it in a toast. - Added receiving single image and displaying it. - Added receiving multiple images

--- app/src/main/java/fakheroueslati/net/datareceiversample/MainActivity.java
+++ app/src/main/java/fakheroueslati/net/datareceiversample/MainActivity.java
@@ -1,5 +1,7 @@
 package fakheroueslati.net.datareceiversample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +10,73 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout mContentMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mContentMain = (LinearLayout)findViewById(R.id.content_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent); // Handle text being sent
+            } else if (type.startsWith("image/")) {
+                handleSendImage(intent); // Handle single image being sent
             }
-        });
+        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendMultipleImages(intent); // Handle multiple images being sent
+            }
+        }
+
+    }
+
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            Toast.makeText(getApplicationContext(),intent.getDataString(),Toast.LENGTH_LONG).show();
+        }
+    }
+
+    void handleSendImage(Intent intent) {
+        Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            addImage(imageUri);
+        }
+    }
+
+    void handleSendMultipleImages(Intent intent) {
+        ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+        if (imageUris != null) {
+            for (Uri imageUri:imageUris) {
+                addImage(imageUri);
+            }
+        }
+    }
+
+    void addImage(Uri imageUri) {
+        //ImageView Setup
+        ImageView imageView = new ImageView(this);
+        //setting image resource
+        imageView.setImageURI(imageUri);
+
+        //adding view to layout
+        mContentMain.addView(imageView);
     }
 
     @Override
