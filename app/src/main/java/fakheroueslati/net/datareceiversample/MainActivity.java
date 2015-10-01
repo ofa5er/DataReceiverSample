1a7978a6d398a847d8759536054392f0416efa96
Fakher Oueslati <oueslati.fakher@gmail.com>
2015-09-30 21:04:09 -0400

    Code refactoring

--- app/src/main/java/fakheroueslati/net/datareceiversample/MainActivity.java
+++ app/src/main/java/fakheroueslati/net/datareceiversample/MainActivity.java
@@ -3,16 +3,12 @@ package fakheroueslati.net.datareceiversample;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.Toast;
 
 import java.util.ArrayList;
